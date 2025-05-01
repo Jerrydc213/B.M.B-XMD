bd2064d6eea69b4f3d5117cf8642548250c9d94b
Jerrydc213 <emmamalay30@gmail.com>
2025-05-01 18:24:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~Za0AyKjR#KE0WIxsePzhe1LTxC-yofKCMpZaCyMZwm2-_YL0rWCA",
+SESSION_ID: process.env.SESSION_ID || "",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
